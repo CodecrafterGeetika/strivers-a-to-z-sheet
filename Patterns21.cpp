fe02ee79ff09acdea8fdc964be5689eb25da3758
CodecrafterGeetika <geetikaranisahu@gmail.com>
2025-08-04 04:51:08 +0530

    Update and rename Patterns.cpp to Patterns21.cpp

---
+++ Patterns21.cpp
@@ -0,0 +1,46 @@
+****
+****
+****
+****
+
+void pattern1(int n) {
+      for (int i = 0; i < n; i++) {
+         for (int j = 0; j < n; j++) {
+          cout << "*";
+         }
+       cout << endl;
+    }
+12345
+1234
+123
+12
+1
+
+void pattern6(int n) {
+for (int i = 0; i< n; i++) {
+      for (int j = 1; j <  n-i+1; j++)  {
+          cout << j;
+      }
+      cout << endl;
+    }
+       
+}
+
+    #
+   ###
+  #####
+ #######
+#########
+void pattern7(int n) {
+  
+}
+
+4444444
+4333334
+4322234
+4321234
+4322234
+4333334
+4444444
+EXPLANATION 
+WE
